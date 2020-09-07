b5a454dceacaaea5f8fe22ea128050a8d72f5f7a
Jomarie Polanco <jomariepolanco@gmail.com>
2020-09-07 16:50:13 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,7 +2,7 @@
 count = 10 
 magic_exit_number = 0 
 
-while count <= 10 && count >= 0 do
+while (count <= 10 && count >= 0) do
   count -= 1
   break if count == magic_exit_number
   puts "Happy New Year!"
